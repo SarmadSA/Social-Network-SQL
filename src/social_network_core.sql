875891fd39a3af1a7380fa2add7013df80fcabe1
Sarmad <sarmadsa2@gmail.com>
2018-10-28 16:25:37 +0100

    Solve Q3 Core

--- src/social_network_core.sql
+++ src/social_network_core.sql
@@ -14,3 +14,12 @@ WHERE (l.ID1 = h1.ID
    AND (l.ID1 = h2.ID
    OR l.ID2 = h2.ID)
    AND h1.grade > (h2.grade + 1)
+
+--Q3 For every pair of students who both like each other, return the name and grade of both students. Include each pair only once, with the two names in alphabetical order.
+SELECT Distinct h1.name, h1.grade, h2.name, h2.grade
+FROM Highschooler h1, Highschooler h2, Likes l, Likes l2
+WHERE h1.ID = l.ID1
+    AND h2.ID = l.ID2
+    AND h1.ID = l2.ID2
+    AND h2.ID = l2.ID1
+    AND h1.name < h2.name
