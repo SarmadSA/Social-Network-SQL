b926db35a36ae1df134ea0dd2d842be3502ac6ed
Sarmad <sarmadsa2@gmail.com>
2018-10-28 16:37:23 +0100

    Solve Q2 Modification

--- src/social_network_modification.sql
+++ src/social_network_modification.sql
@@ -2,3 +2,15 @@
 
 --Q1 It's time for the seniors to graduate. Remove all 12th graders from Highschooler.
 DELETE FROM Highschooler WHERE grade = 12
+
+--Q2 If two students A and B are friends, and A likes B but not vice-versa, remove the Likes tuple.
+DELETE FROM Likes
+WHERE ID1 IN
+(SELECT l.ID1
+FROM Friend f, Likes l, Likes l2
+WHERE l.ID1 = f.ID1
+AND l.ID2 = f.ID2
+EXCEPT SELECT l.ID1
+    FROM Friend f, Likes l, Likes l2
+    WHERE l.ID2 = l2.ID1
+    AND l2.ID2 = l.ID1)
