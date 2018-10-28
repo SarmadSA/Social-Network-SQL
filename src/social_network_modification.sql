0cd8cc2e7a3c89a1cb15168bf1ad50e4fd955dc3
Sarmad <sarmadsa2@gmail.com>
2018-10-28 16:38:31 +0100

    Solve Q3 Modification

--- src/social_network_modification.sql
+++ src/social_network_modification.sql
@@ -14,3 +14,31 @@ EXCEPT SELECT l.ID1
     FROM Friend f, Likes l, Likes l2
     WHERE l.ID2 = l2.ID1
     AND l2.ID2 = l.ID1)
+
+--Q3 For all cases where A is friends with B, and B is friends with C, add a new friendship for the pair A and C. Do not add duplicate friendships, friendships that already exist, or friendships with oneself.
+INSERT INTO Friend
+SELECT a.ID, c.ID
+FROM Highschooler a, Highschooler b, Highschooler c, Friend f, Friend f2
+WHERE a.ID = f.ID1
+    AND b.ID = f.ID2
+    AND b.ID = f2.ID1
+    AND c.ID = f2.ID2
+    AND a.ID <> c.ID
+    EXCEPT
+    SELECT a.ID, c.ID
+    FROM Highschooler a, Highschooler c, Friend f
+    WHERE a.ID = f.ID1
+        AND c.ID = f.ID2
+
+-------- OR --------
+
+INSERT INTO Friend
+SELECT a.ID, c.ID
+FROM Highschooler a, Highschooler b, Highschooler c, Friend f, Friend f2
+WHERE a.ID = f.ID1
+    AND b.ID = f.ID2
+    AND b.ID = f2.ID1
+    AND c.ID = f2.ID2
+    AND a.ID <> c.ID
+    EXCEPT
+    SELECT * FROM Friend
